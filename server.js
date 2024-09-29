75c7156b1a170bff7cc0f979bad9515d53e61906
Alan Silva dos Santos <145307753+Alan-coll-821@users.noreply.github.com>
2024-09-29 18:25:59 -0300

    teste
    teste camera

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('Novo cliente conectado');
+
+    socket.on('stream', (data) => {
+        socket.broadcast.emit('stream', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
